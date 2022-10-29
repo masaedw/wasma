d6b2fd3c2595c8ac4c3f266a19fb17945e48887c
Masayuki Muto <masayuki.muto@uzabase.com>
2022-10-29 13:44:26 +0900

    add test

---
+++ src/test/kotlin/Module.kt
@@ -0,0 +1,22 @@
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+/**
+ * convert hex string separated by space to UByteArray
+ */
+fun String.decodeHex(): ByteArray {
+    val dense = this.replace(" ", "")
+
+    return ByteArray(dense.length / 2) {
+        Integer.parseInt(dense, it * 2, (it + 1) * 2, 16).toByte()
+    }
+}
+
+class ModuleTest : FunSpec({
+    context("load") {
+        test("decodeHex") {
+            val bytes = "00 61 73 6d 01 00 00 00".decodeHex()
+            bytes shouldBe byteArrayOf(0x00, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00)
+        }
+    }
+})
